21f02955ef2315ccd3473af96ce0641b7bacce26
Scottie Crump <sncrump29@gmail.com>
2019-05-15 14:39:51 -0400

    refactor(backend): update endpoint urls

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/laptop', laptopRouter);
+app.use('/api/', indexRouter);
+app.use('/api/laptop', laptopRouter);
 
 module.exports = app;
